932aa12c47c66a73f7d053a474100f3947316101
Afonso Matumona <afonso.matumona@minfin.gov.ao>
2021-12-02 11:47:09 +0100

    Minor changes made

--- src/shared/helpers/Constants.ts
+++ src/shared/helpers/Constants.ts
@@ -1,7 +1,7 @@
 import { startWith } from "./Utils";
 
 export const Constants = {
-  ignore: 'e-ignore',
+  skip: 'e-skip',
 
   build: 'e-build',
   array: 'e-array',
@@ -12,29 +12,20 @@ export const Constants = {
   show: 'e-show',
 
   req: 'e-req',
-  tmp: 'e-tmp',
-  fill: 'e-fill',
   for: 'e-for',
-  use: 'e-use',
-  id: 'e-id',
-  order: 'e-order',
-  filter: 'e-filter',
 
   data: 'data',
   def: 'e-def',
   wait: 'wait-data',
 
-  toggle: 'e-toggle',
   content: 'e-content',
   bind: 'e-bind',
   property: 'e-',
 
-  anm: 'e-anm',
   skeleton: 'e-skeleton',
 
   route: 'route-view',
   href: ':href',
-  ihref: '!href',
 
   entry: 'e-entry',
 
@@ -43,9 +34,7 @@ export const Constants = {
   silent: 'silent',
   slot: 'slot',
   ref: 'ref',
-  put: 'put',
-
-  connector: 'connector',
+  put: 'e-put',
 
   builtInEvents: {
     add: 'add',
