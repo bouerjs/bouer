20100d0f0ea780535df3a52feaae8d63ab0ce384
Afonso Matumona <afonso.matumona@minfin.gov.ao>
2021-12-02 11:46:12 +0100

    Implementation modified

--- src/core/middleware/Middleware.ts
+++ src/core/middleware/Middleware.ts
@@ -1,33 +1,83 @@
 import IoC from "../../shared/helpers/IoC";
 import Logger from "../../shared/logger/Logger";
-import dynamic from "../../types/dynamic";
 import IMiddleware from "../../types/IMiddleware";
 
+export type MiddlewareObject = {
+	bind?: (context: IMiddleware, next: () => void) => Promise<any>,
+	update?: (context: IMiddleware, next: () => void) => Promise<any>
+}
+
+export type MiddlewareConfig = (
+	bind: (configure: (context: IMiddleware) => Promise<any>) => void,
+	update: (configure: (context: IMiddleware) => Promise<any>) => void
+) => void;
+
 export default class Middleware {
-  private container: dynamic = {};
+	private middlewareConfigContainer: { [key: string]: MiddlewareObject[] } = {};
 
-  constructor() {
-    IoC.Register(this);
-  }
+	constructor() {
+		IoC.Register(this);
+	}
 
-  run = (action: string, options: IMiddleware) => {
-    const middlewares = this.container[action];
-    if (!middlewares) return Logger.log("There is not middleware for “" + action + "”.");
+	run = (directive: string, runnable: {
+		type: 'bind' | 'update',
+		action: (middleware: (context: IMiddleware, callbacks: {
+			success: (response: any) => void,
+			fail: (error: any) => void,
+			done: () => void
+		}) => void) => void,
+		default?: () => void
+	}) => {
 
+		const middlewares = this.middlewareConfigContainer[directive];
+		if (!middlewares) {
+			return (runnable.default || (() => { }))();
+		};
 
-    for (let index = 0; index < middlewares.length; index++) {
-      const middleware = middlewares[index];
-      middleware(options);
-    }
-  }
+		let index = 0;
+		let middleware = middlewares[index];
 
-  configure = (action: string, callback: (options: IMiddleware) => void) => {
-    let mMiddlewares = this.container[action];
+		while (middleware != null) {
+			let isNext = false;
+			const middlewareAction = middleware[runnable.type];
 
-    if (!mMiddlewares) {
-      return this.container[action] = [ callback ];
-    }
+			if (middlewareAction) {
+				runnable.action((config, cbs) => {
+					Promise.resolve(middlewareAction(config, () => {
+						isNext = true;
+					})).then(value => {
+						if (isNext) return;
+						cbs.success(value);
+					}).catch(error => {
+						if (isNext) return;
+						cbs.fail(error);
+					}).finally(() => {
+						if (isNext) return;
+						cbs.done();
+					});
+				});
+			} else {
+				(runnable.default || (() => { }))();
+			}
 
-    mMiddlewares.push(callback);
-  }
+			if (isNext == false)
+				break;
+
+			middleware = middlewares[++index];
+		}
+	}
+
+	register = (directive: string, configureCallback: MiddlewareConfig) => {
+		if (!this.middlewareConfigContainer[directive])
+			this.middlewareConfigContainer[directive] = [];
+
+		const middleware: MiddlewareObject = {};
+
+		configureCallback(
+			bind => middleware.bind = bind,
+			update => middleware.update = update
+		);
+
+		this.middlewareConfigContainer[directive].push(middleware);
+	}
 }
