ba93cf86a8e89d7c26340355e0bacee4a68d629a
Afonso Matumona <afonso.matumona@minfin.gov.ao>
2022-02-06 12:44:48 +0100

    Added generated date-time when the files a being transpiled

--- scripts/build.js
+++ scripts/build.js
@@ -6,62 +6,65 @@ const terser = require('terser');
 const builds = require('./config').builds;
 
 Object.keys(builds).filter(key => {
-    const config = builds[key];
-    const output = config.output;
+	const config = builds[key];
+	const output = config.output;
 
-    const fileNameDev = output.file;
-    const fileNameProd = fileNameDev.replace('.js', '.min.js');
-    const isProd = process.argv[2] === '--prod';
+	const fileNameDev = output.file;
+	const fileNameProd = fileNameDev.replace('.js', '.min.js');
+	const isProd = process.argv[2] === '--prod';
 
-    if (!fs.existsSync('dist')) {
-      fs.mkdirSync('dist')
-    }
+	if (!fs.existsSync('dist')) {
+		fs.mkdirSync('dist')
+	}
 
-    rollup.rollup(config)
-      .then(bundle => bundle.generate(output))
-      .then(({ output: [{ code }] }) => {
-        writeFile(fileNameDev, code);
+	rollup.rollup(config)
+		.then(bundle => bundle.generate(output))
+		.then(({ output: [{ code }] }) => {
+			writeFile(fileNameDev, code);
 
-        if (!isProd) return;
-        terser.minify(code, {
-          toplevel: true,
-          output: {
-            ascii_only: true
-          },
-          compress: {
-            pure_funcs: ['makeMap']
-          }
-        }).then(terserOutput => {
-          return writeFile(fileNameProd, terserOutput.code, true)
-        });
-      })
-      .catch(err => {
-        console.error(err);
-      });
+			if (!isProd) return;
+			terser.minify(code, {
+				toplevel: true,
+				output: {
+					ascii_only: true
+				},
+				compress: {
+					pure_funcs: ['makeMap']
+				}
+			}).then(terserOutput => {
+				return writeFile(fileNameProd, terserOutput.code, true)
+			});
+		})
+		.catch(err => {
+			console.error(err);
+		});
 });
 
 function writeFile(dest, code, zip) {
-  return new Promise((resolve, reject) => {
-    function report(extra) {
-      const colored = `\x1b[1m\x1b[34m${(path.relative(process.cwd(), dest)) + ' ' + getSize(code) + (extra || '')}\x1b[39m\x1b[22m`;
-      console.log(colored);
-      resolve()
-    }
+	return new Promise((resolve, reject) => {
+		function report(extra) {
+			const colored = `\x1b[1m\x1b[34m${(path.relative(process.cwd(), dest)) + ' ' + getSize(code) + (extra || '')}\x1b[39m\x1b[22m`;
+			const instance = new Date();
+			const date = instance.toJSON().split('T')[0];
+			const time = instance.toLocaleTimeString('pt');
+			console.log(`[${ date } ${ time }] ${colored}`);
+			resolve()
+		}
 
-    fs.writeFile(dest, code, err => {
-      if (err) return reject(err)
-      if (zip) {
-        zlib.gzip(code, (err, zipped) => {
-          if (err) return reject(err)
-          report(' (gzipped: ' + getSize(zipped) + ')')
-        })
-      } else {
-        report()
-      }
-    })
-  })
+		fs.writeFile(dest, code, err => {
+			if (err) return reject(err)
+			if (zip) {
+				zlib.gzip(code, (err, zipped) => {
+					if (err) return reject(err)
+					report(' (gzipped: ' + getSize(zipped) + ')')
+				})
+			} else {
+				report()
+			}
+		})
+	})
 }
 
 function getSize(code) {
-  return (code.length / 1024).toFixed(2) + 'kb'
+	return (code.length / 1024).toFixed(2) + 'kb'
 }
