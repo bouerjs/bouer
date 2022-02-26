6598cf656ddc50a0ac5510bdbf3c7ad2bb4963e7
AfonsoMatElias <afonsomatumona@hotmail.com>
2022-02-26 15:29:21 +0100

    feat: added shorthand attribuite check

--- src/core/DelimiterHandler.ts
+++ src/core/DelimiterHandler.ts
@@ -6,54 +6,64 @@ import { isNull, trim } from "../shared/helpers/Utils";
 import Base from "./Base";
 
 export default class DelimiterHandler extends Base {
-  delimiters: Array<IDelimiter> = [];
+	delimiters: Array<IDelimiter> = [];
 	bouer: Bouer;
 
-  constructor(delimiters: Array<IDelimiter>, bouer: Bouer) {
+	constructor(delimiters: Array<IDelimiter>, bouer: Bouer) {
 		super();
 
 		this.bouer = bouer;
-    this.delimiters = delimiters;
-    ServiceProvider.add('DelimiterHandler', this);
-  }
+		this.delimiters = delimiters;
+		ServiceProvider.add('DelimiterHandler', this);
+	}
 
-  add(item: IDelimiter) {
-    this.delimiters.push(item);
-  }
+	add(item: IDelimiter) {
+		this.delimiters.push(item);
+	}
 
-  remove(name: string) {
-    const index = this.delimiters.findIndex(item => item.name === name);
-    this.delimiters.splice(index, 1);
-  }
+	remove(name: string) {
+		const index = this.delimiters.findIndex(item => item.name === name);
+		this.delimiters.splice(index, 1);
+	}
 
-  run(content: string): IDelimiterResponse[] {
-    if (isNull(content) || trim(content) === '') return [];
-    let mDelimiter: IDelimiter | null = null;
+	run(content: string): IDelimiterResponse[] {
+		if (isNull(content) || trim(content) === '') return [];
+		let mDelimiter: IDelimiter | null = null;
 
-    const checkContent = (text: string, flag?: string): RegExpMatchArray | undefined => {
-      const center = '([\\S\\s]*?)';
-      for (var i = 0; i < this.delimiters.length; i++) {
-        const delimiter = this.delimiters[i];
-        const result = text.match(
-          RegExp(delimiter.delimiter.open + center + delimiter.delimiter.close, flag || '')
-        );
-        if (result) {
-          mDelimiter = delimiter;
-          return result;
-        }
-      }
-    }
+		const checkContent = (text: string, flag?: string): RegExpMatchArray | undefined => {
+			const center = '([\\S\\s]*?)';
+			for (var i = 0; i < this.delimiters.length; i++) {
+				const delimiter = this.delimiters[i];
+				const result = text.match(
+					RegExp(delimiter.delimiter.open + center + delimiter.delimiter.close, flag || '')
+				);
+				if (result) {
+					mDelimiter = delimiter;
+					return result;
+				}
+			}
+		}
 
-    const result = checkContent(content, 'g');
-    if (!result) return [];
+		const result = checkContent(content, 'g');
+		if (!result) return [];
 
-    return result.map(function (item) {
-      const matches = checkContent(item) as RegExpMatchArray;
-      return {
-        field: matches[0],
-        expression: trim(matches[1]),
-        delimiter: mDelimiter!
-      }
-    });
-  }
+		return result.map(function (item) {
+			const matches = checkContent(item) as RegExpMatchArray;
+			return {
+				field: matches[0],
+				expression: trim(matches[1]),
+				delimiter: mDelimiter!
+			}
+		});
+	}
+
+	shorthand(attrName: string): IDelimiterResponse  | null {
+		if (isNull(attrName) || trim(attrName) === '') return null;
+		const result = attrName.match(new RegExp("{([\\w{$,-}]*?)}"));
+		if (!result) return null;
+		return {
+			field: result[0],
+			expression: trim(result[1])
+		}
+	}
 }
