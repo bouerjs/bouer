695236ab4976442e29204b29b2bc1e625b6f8aa6
AfonsoMatElias <afonsomatumona@hotmail.com>
2022-03-15 17:57:25 +0100

    refactor: modified the console args application

--- src/shared/logger/Logger.ts
+++ src/shared/logger/Logger.ts
@@ -2,15 +2,19 @@ export default class Logger {
   private static prefix = '[Bouer]';
 
   static log(...content: any[]) {
-    console.log(Logger.prefix, ...content);
+    content.unshift(Logger.prefix);
+    console.log.apply(null, content);
   }
   static error(...content: any[]) {
-    console.error(Logger.prefix, ...content);
+    content.unshift(Logger.prefix);
+    console.error.apply(null, content);
   }
   static warn(...content: any[]) {
-    console.warn(Logger.prefix, ...content);
+    content.unshift(Logger.prefix);
+    console.warn.apply(null, content);
   }
   static info(...content: any[]) {
-    console.info(Logger.prefix, ...content);
+    content.unshift(Logger.prefix);
+    console.info.apply(null, content);
   }
 }
