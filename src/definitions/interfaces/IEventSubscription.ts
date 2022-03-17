c659b5e95f20ea74c2a14f258dddcae3f40e9bcb
AfonsoMatElias <afonsomatumona@hotmail.com>
2022-03-17 15:16:35 +0100

    feat: enabled Promise in event subscription callback

--- src/definitions/interfaces/IEventSubscription.ts
+++ src/definitions/interfaces/IEventSubscription.ts
@@ -4,7 +4,7 @@ interface IEventSubscription {
   eventName: string
   attachedNode?: Node,
   modifiers?: IEventModifiers,
-  callback: (evt: Event | CustomEvent, ...args: any[]) => void,
+  callback: (evt: Event | CustomEvent, ...args: any[]) => void | Promise<any>,
   emit: (options?: { init?: CustomEventInit, once?: boolean, }) => void
 }
 
