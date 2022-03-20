8e163001b0f7ce2aa564fb8d0dcef49ea72701ef
AfonsoMatElias <afonsomatumona@hotmail.com>
2022-03-20 23:42:44 +0100

    test: added test for set method

---
+++ test/unit/method/set.test.js
@@ -0,0 +1,43 @@
+import {
+  Bouer,
+} from '../../index';
+
+describe('When "set" method is called with no target object', () => {
+  let context;
+
+  beforeEach(() => {
+    context = Bouer.create();
+  });
+
+  it('Adds all the properties the instance data', () => {
+    context.set({
+      username: 'AfonsoMatElias'
+    });
+    expect('username' in context.data).toBe(true);
+    expect(context.data.username).toBe('AfonsoMatElias');
+  });
+});
+
+describe('When "set" method is called with target object', () => {
+  let context;
+
+  beforeEach(() => {
+    context = Bouer.create({
+      data: {
+        person: {
+          name: 'Elias',
+          country: 'Angola'
+        }
+      }
+    });
+  });
+
+  it('Adds all the properties the instance data', () => {
+    context.set({
+      username: 'AfonsoMatElias'
+    }, context.data.person);
+
+    expect('username' in context.data).toBe(false);
+    expect('username' in context.data.person).toBe(true);
+  });
+});
