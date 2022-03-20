ac06ef20d7e4ff33a2e77d9555f582c1b5a1ae86
AfonsoMatElias <afonsomatumona@hotmail.com>
2022-03-20 23:38:25 +0100

    test: added test for e-def directive

---
+++ test/unit/directive/e-def.test.js
@@ -0,0 +1,56 @@
+import {
+  Bouer,
+  Compiler,
+  toHtml
+} from '../../index';
+
+describe('When element is compiled with "e-def" directive', () => {
+  let context;
+  let compiler;
+
+  beforeEach(() => {
+    context = Bouer.create();
+    compiler = new Compiler(context, {});
+  });
+
+  const htmlSnippet = '<h4 e-def="{ prop: \'new\' }"></h4>';
+
+  it('Adds property to the current scope', () => {
+    const element = toHtml(htmlSnippet);
+
+    compiler.compile({
+      data: context.data,
+      context: context,
+      el: element,
+      onDone: el => {
+        expect('prop' in context.data).toBe(true);
+      }
+    });
+  });
+
+  it('Added properties have the exact value', () => {
+    const element = toHtml(htmlSnippet);
+
+    compiler.compile({
+      data: context.data,
+      context: context,
+      el: element,
+      onDone: _ => {
+        expect(context.data.prop).toEqual('new');
+      }
+    });
+  });
+
+  it('Removes the directive after compilation', () => {
+    const element = toHtml(htmlSnippet);
+
+    compiler.compile({
+      data: context.data,
+      context: context,
+      el: element,
+      onDone: el => {
+        expect(el.hasAttribute('e-def')).toBe(false);
+      }
+    });
+  });
+});
