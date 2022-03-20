72a32f7d53c6665a426ce4dc430e681157e5e5df
AfonsoMatElias <afonsomatumona@hotmail.com>
2022-03-20 23:39:15 +0100

    test: added test for e-skip directive

---
+++ test/unit/directive/e-skip.test.js
@@ -0,0 +1,38 @@
+import {
+  Bouer,
+  Compiler,
+  sleep,
+  toHtml
+} from '../../index';
+
+describe('When element is compiled with "e-skip" directive', () => {
+  let context;
+  let compiler;
+
+  beforeEach(() => {
+    context = Bouer.create({
+      data: {
+        value: 'value-rendered'
+      }
+    });
+    compiler = new Compiler(context, {});
+  });
+
+  const htmlSnippet = '<h4 e-skip>{{ value }}</h4>';
+
+  it('Ignore the element compilation', async () => {
+    const element = toHtml(htmlSnippet);
+
+    compiler.compile({
+      data: context.data,
+      context: context,
+      el: element,
+      onDone: el => {
+        expect(el.textContent).toContain('{{ value }}');
+      }
+    });
+
+    await sleep(5);
+    expect(element.textContent).toContain('{{ value }}');
+  });
+});
