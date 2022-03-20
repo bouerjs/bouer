7418352105abfbd31d67a04d89fed5c0f61aa32f
AfonsoMatElias <afonsomatumona@hotmail.com>
2022-03-20 23:39:24 +0100

    test: added test for e-text directive

---
+++ test/unit/directive/e-text.test.js
@@ -0,0 +1,40 @@
+import {
+  Bouer,
+  Compiler,
+  toHtml
+} from '../../index';
+
+describe('When element is compiled with "e-text" directive', () => {
+  const context = Bouer.create({
+    data: {
+      message: 'Hello'
+    }
+  });
+  const compiler = new Compiler(context, {});
+
+  it('Renders the data property value in the element content', () => {
+    const element = toHtml('<h4 e-text="{{ message }}">#</h4>');
+
+    compiler.compile({
+      data: context.data,
+      context: context,
+      el: element,
+      onDone: el => {
+        expect(el.textContent).toContain('Hello');
+      }
+    });
+  });
+
+  it('Removes the directive after compilation', () => {
+    const element = toHtml('<h4 e-text="{{ message }}">#</h4>');
+
+    compiler.compile({
+      data: context.data,
+      context: context,
+      el: element,
+      onDone: el => {
+        expect(el.hasAttribute('e-text')).toBe(false);
+      }
+    });
+  });
+});
