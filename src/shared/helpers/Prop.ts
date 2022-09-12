e1e1a393fef62861b42fe7ae30189ad3025e31e5
AfonsoMatElias <afonsomatumona@hotmail.com>
2022-09-12 14:28:26 +0100

    chore: fixed typing error

--- src/shared/helpers/Prop.ts
+++ src/shared/helpers/Prop.ts
@@ -1,25 +1,27 @@
 import Reactive from '../../core/reactive/Reactive';
 
 export default class Prop {
-  static set<T extends {}>(obj: T, propName: string, descriptor: PropertyDescriptor) {
+  static set<T extends {}, TKey extends keyof T>(
+    obj: T, propName: TKey, descriptor: PropertyDescriptor
+  ) {
     Object.defineProperty(obj, propName, descriptor);
     return obj;
   }
 
-  static descriptor<T extends {}>(obj: T, propName: string) {
+  static descriptor<T extends {}, TKey extends keyof T>(obj: T, propName: TKey) {
     return Object.getOwnPropertyDescriptor(obj, propName);
   }
 
-  static transfer<Source extends {}, Destination extends {}>(
-    destination: Source,
-    source: Destination,
-    propName: string
+  static transfer<Source extends {}, Destination extends {}, TKey extends keyof Source>(
+    destination: Destination,
+    source: Source,
+    propName: TKey
   ) {
     const descriptor = Prop.descriptor(source, propName) as Reactive<any, Source>;
     const mDst = (destination as any);
     if (!(propName in destination))
       mDst[propName] = undefined;
 
-    Prop.set(destination, propName, descriptor);
+    Prop.set(destination, propName as any, descriptor);
   }
 }
