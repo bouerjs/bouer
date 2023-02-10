58ee424deefec28eaa7b1f78bbaf744e4e203430
AfonsoMatElias <afonsomatumona@hotmail.com>
2023-02-10 07:43:42 +0100

    feat: added ViewChild to be able to retrieve an active component

---
+++ src/core/ViewChild.ts
@@ -0,0 +1,34 @@
+import Bouer from '../instance/Bouer';
+import ServiceProvider from '../shared/helpers/ServiceProvider';
+import { getRootElement, where } from '../shared/helpers/Utils';
+import Component from './component/Component';
+import ComponentHandler from './component/ComponentHandler';
+
+export default class ViewChild {
+  static viewBy<Child extends Component>(bouerInstance: Bouer, expression: (component: Component) => boolean):
+    Child[] {
+    // Retrieving the active component
+    const activeComponents = ServiceProvider.get<ComponentHandler>(bouerInstance, 'ComponentHandler')
+      .activeComponents as any[];
+    // Applying filter to the find the component
+    return where(activeComponents, expression) as Child[];
+  }
+
+  static viewById<Child extends Component>(bouerInstance: Bouer, componentId: string):
+    Child[] {
+    // Retrieving the active component
+    const activeComponents = ServiceProvider.get<ComponentHandler>(bouerInstance, 'ComponentHandler')
+      .activeComponents;
+    // Applying filter to the find the component
+    return where(activeComponents, c => (c.el && getRootElement(c.el).id == componentId)) as Child[];
+  }
+
+  static viewByName<Child extends Component>(bouerInstance: Bouer, componentName: string):
+    Child[] {
+    // Retrieving the active component
+    const activeComponents = ServiceProvider.get<ComponentHandler>(bouerInstance, 'ComponentHandler')
+      .activeComponents;
+    // Applying filter to the find the component
+    return where(activeComponents, c => c.name.toLowerCase() == (componentName || '').toLowerCase()) as Child[];
+  }
+}
