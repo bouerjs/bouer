72140a08f568eae4c66dd070a0d07c192e881101
AfonsoMatElias <afonsomatumona@hotmail.com>
2023-04-07 10:14:45 +0100

    chore: added an extends to Data generic parameter

--- src/definitions/types/RenderContext.ts
+++ src/definitions/types/RenderContext.ts
@@ -1,6 +1,7 @@
 import Component from '../../core/component/Component';
 import Bouer from '../../instance/Bouer';
+import dynamic from './Dynamic';
 
-type RenderContext<Data = {}> = Bouer<Data, {}, {}> | Component<Data>;
+type RenderContext<Data extends dynamic = {}> = Bouer<Data, {}, {}> | Component<Data>;
 
 export default RenderContext;
