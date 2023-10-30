abd0f0f944b43fad980f674b8c2d1722876b2230
AfonsoMatElias <afonsomatumona@hotmail.com>
2023-10-30 11:10:11 +0100

    new example for component

---
+++ examples/components/app.js
@@ -0,0 +1,12 @@
+var app = new Bouer('#app', {
+  components: [
+    {
+      name: 'app-header',
+      path: '/components/app-header.html'
+    },
+    {
+      name: 'app-footer',
+      path: '/components/app-footer.html'
+    }
+  ]
+});
---
+++ test/usage/ts/src/components/social/Social.ts
@@ -0,0 +1,8 @@
+import { Component } from 'bouerjs';
+import html from './Social.html';
+
+export default class Social extends Component {
+  constructor() {
+    super(html);
+  }
+}
