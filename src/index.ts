61e8165b34ea663bd3e4e0deacf0c5cb81a2b7e7
AfonsoMatElias <afonsomatumona@hotmail.com>
2023-10-30 10:47:28 +0100

    feat: Utils functions exported to be used in the applications

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,33 @@
 export { default as default } from './instance/Bouer';
 
 export { default as Component } from './core/component/Component';
-export { default as IBouerOptions } from './definitions/interfaces/IBouerOptions';
-export { default as IComponentOptions } from './definitions/interfaces/IComponentOptions';
-export { default as Watch } from './core/binder/Watch';
 export { default as Reactive } from './core/reactive/Reactive';
-export { default as IBinderConfig } from './definitions/interfaces/IBinderConfig';
-export { default as IDelimiter } from './definitions/interfaces/IDelimiter';
-export { default as IDelimiterResponse } from './definitions/interfaces/IDelimiterResponse';
-export { default as IMiddleware } from './definitions/interfaces/IMiddleware';
-export { default as WatchCallback } from './definitions/types/WatchCallback';
-export { default as CustomDirective } from './definitions/types/CustomDirective';
-export { default as IEventModifiers } from './definitions/interfaces/IEventModifiers';
-export { default as IEventSubscription } from './definitions/interfaces/IEventSubscription';
-export { default as IAsset } from './definitions/interfaces/IAsset';
-export { default as MiddlewareResult } from './core/middleware/MiddlewareResult';
+export { default as Compiler } from './core/compiler/Compiler';
+export { default as Routing } from './core/routing/Routing';
+export { default as Watch } from './core/binder/Watch';
 export { default as Extend } from './shared/helpers/Extend';
 export { default as Prop } from './shared/helpers/Prop';
 export { default as ViewChild } from './core/ViewChild';
 export { default as IoC } from './shared/helpers/IoCContainer';
+
+export { default as IBouerOptions } from './definitions/interfaces/IBouerOptions';
+export { default as IBouerConfig } from './definitions/interfaces/IBouerConfig';
+export { default as IComponentOptions } from './definitions/interfaces/IComponentOptions';
+export { default as IBinderConfig } from './definitions/interfaces/IBinderConfig';
+export { default as IDelimiter } from './definitions/interfaces/IDelimiter';
+export { default as IDelimiterResponse } from './definitions/interfaces/IDelimiterResponse';
+export { default as IMiddleware } from './definitions/interfaces/IMiddleware';
+export { default as IEventModifiers } from './definitions/interfaces/IEventModifiers';
+export { default as IEventSubscription } from './definitions/interfaces/IEventSubscription';
+export { default as IEventEmitterOptions } from './definitions/interfaces/IEventEmitterOptions';
+export { default as IAsset } from './definitions/interfaces/IAsset';
+export { default as IMiddlewareResult } from './core/middleware/IMiddlewareResult';
+
+export { default as dynamic } from './definitions/types/Dynamic';
+export { default as CustomDirective } from './definitions/types/CustomDirective';
+export { default as RenderContext } from './definitions/types/RenderContext';
+export { default as SkeletonOptions } from './definitions/types/SkeletonOptions';
+export { default as WatchCallback } from './definitions/types/WatchCallback';
+export { default as DataType } from './definitions/types/DataType';
+
+export * from './shared/helpers/Utils';
