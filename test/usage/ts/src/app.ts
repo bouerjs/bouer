6b977626c0f0ba607d81315ad27b08d70846955d
AfonsoMatElias <afonsomatumona@hotmail.com>
2023-11-01 14:03:16 +0100

    component registration as class type

--- test/usage/ts/src/app.ts
+++ test/usage/ts/src/app.ts
@@ -2,6 +2,7 @@ import Bouer from 'bouerjs';
 import AppHeader from './components/header/AppHeader';
 import AppMain from './components/main/AppMain';
 import AppFooter from './components/footer/AppFooter';
+import Social from './components/social/Social';
 
 import './index.html';
 import './main.scss';
@@ -10,10 +11,14 @@ new Bouer('#app', {
   data: {
     // Props
     version: '3.0.0',
+    func() {
+      // const _ =  this.data;
+    }
   },
   components: [
-    new AppHeader(),
-    new AppMain(),
-    new AppFooter(),
+    AppHeader,
+    AppMain,
+    AppFooter,
+    Social
   ],
 });
