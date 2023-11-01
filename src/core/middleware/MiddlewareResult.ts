fe492664e9bc2c368f03e86e7046aa765129b595
AfonsoMatElias <afonsomatumona@hotmail.com>
2023-11-01 12:01:07 +0100

    removed unnecessary file

--- src/core/middleware/MiddlewareResult.ts
+++
@@ -1,8 +0,0 @@
-interface MiddlewareResult<T = any> {
-  /** The data that will be used in the directive */
-  data: T,
-  /** An additional fields */
-  [key: string]: any
-}
-
-export default MiddlewareResult;
