d4dd490042479e19bf1de63dc54e8e9134a87c58
AfonsoMatElias <afonsomatumona@hotmail.com>
2023-11-01 14:05:21 +0100

    IoC logic modified

--- src/shared/helpers/IoCContainer.ts
+++ src/shared/helpers/IoCContainer.ts
@@ -2,131 +2,76 @@ import Evaluator from '../../core/Evaluator';
 import dynamic from '../../definitions/types/Dynamic';
 import Bouer from '../../instance/Bouer';
 import Logger from '../logger/Logger';
-import { forEach, ifNullReturn, isNull, toLower, where } from './Utils';
+import { forEach, ifNullReturn } from './Utils';
 
 type ServiceClass<Service> = (new (...args: any[]) => Service);
 type ServiceItem<Service> = {
   clazz: ServiceClass<Service>,
   instance?: Service,
   isSingleton: boolean,
-  args?: dynamic
+  args?: unknown[]
 };
 
 /**
- * It's a **Service Provider** that injects the dependencies passed into the **constructor** of classes that are
- * create via **IoC**.
- *
- * Note: This Service Provider, uses a technic of matching the parameter name (not the Paramenter Type) of the
- * class provided in the constructor. If the parameter name does not match the name of **Class Type**, the IoC
- * resolver won't be able to inject the service.
+ * It's a **Service Provider** container with all the services that will be used in the application.
  */
 export default (function IoC() {
   let bouerId: number = 1;
-  const serviceCollection: Map<Bouer, dynamic<ServiceItem<any>>> = new Map();
+  const serviceCollection: WeakMap<Bouer, WeakMap<ServiceClass<unknown>, ServiceItem<unknown>>> = new WeakMap();
 
-  const findServiceByName = (app: Bouer | null, name: string) => {
-    if (!app) return null;
-
-    const collection = serviceCollection.get(app) || {};
-
-    const serviceName = where(Object.keys(collection),
-      key => toLower(collection[key].clazz.name) == toLower(name)
-    )[0];
-
-    if (!serviceName)
-      return null;
-
-    return collection[serviceName];
-  };
-
-  const classParams = <Type>(ctor: (new (...args: any[]) => Type)) => {
-    if (!ctor) return [];
-    return (ctor + '')
-      .replace(/[/][/].*$/mg, '') // strip single-line comments
-      .replace(/\s+/g, '') // strip white space
-      .replace(/[/][*][^/*]*[*][/]/g, '') // strip multi-line comments
-      .split('){', 1)[0].replace(/^[^(]*[(]/, '') // extract the parameters
-      .replace(/=[^,]+/g, '') // strip any ES6 defaults
-      .split(',').filter(Boolean); // split & filter [""]
-  };
-
-  const add = <Service>(app: Bouer, clazz: ServiceClass<Service>, params?: dynamic, isSingleton?: boolean) => {
+  const add = <Service>(app: Bouer, clazz: ServiceClass<Service>, params?: unknown[], isSingleton?: boolean) => {
     if (app.isDestroyed) throw new Error('Application already disposed.');
 
     if (!serviceCollection.has(app))
-      serviceCollection.set(app, {});
+      serviceCollection.set(app, new WeakMap());
 
-    const serviceName = (clazz as ServiceClass<Service>).name;
     const collection = serviceCollection.get(app)!;
 
-    collection[serviceName] = {
+    collection.set(clazz, {
       clazz: clazz as ServiceClass<Service>,
       isSingleton: ifNullReturn(isSingleton, false),
       args: params
-    };
+    });
   };
 
-  /**
-   * Resolve and Retrieve the instance registered
-   * @param name the name of the service
-   * @returns the instance of the class
-   */
   const resolve = <Service>(app: Bouer, clazz: (new (...args: any[]) => Service)) => {
     if (app.isDestroyed) throw new Error('Application already disposed.');
 
     const collection = serviceCollection.get(app);
     if (!collection) return null;
 
-    const service = collection[clazz.name];
+    const service = collection.get(clazz);
+
+    if (service == null)
+      return null;
+
     if (service.isSingleton) {
 
       if (service.instance)
         return service.instance as Service;
 
       // Otherwise, creates the singleton instance
-      return service.instance = newInstace(app, clazz, service.args) as Service;
+      return service.instance = newInstance(clazz, service.args) as Service;
     }
 
-    return newInstace(app, clazz, service.args);
+    return newInstance(clazz, service.args);
   };
 
   /**
    * Creates a new instance of a class provided
    * @param clazz the class that the new instance should be created
+   * @param params the parameter list that will be injected in the constructor
    * @returns new intance of the class provided
    */
-  const newInstace = <Service>(app: Bouer | null, clazz: (new (...args: any[]) => Service), params?: dynamic) => {
+  const newInstance = <Service>(clazz: (new (...args: any[]) => Service), params?: unknown[]) => {
     const paramsToProvide: string[] = [];
-    const mParams: dynamic = params || {};
-    const data: { __ctor0: ServiceClass<Service>, [key: string]: any } = { __ctor0: clazz };
+    const mParams: unknown[] = params || [];
+    const data: { __ctor0: ServiceClass<Service>, [key: string]: unknown } = { __ctor0: clazz };
 
-    // Looping all the params of the class constructor
-    forEach(classParams(clazz), (key, index) => {
-      // Trying to get the value from the provided params
-      let paramValue = mParams[key];
+    // Looping all the provided params of the class constructor
+    forEach(mParams, (paramValue, index) => {
       // Creating a unique name for the argument
       const paramName = '__arg' + index;
-
-      // If the parameter name matches Bouer, then injects the provided bouer instance
-      if (toLower(Bouer.name) === toLower(key)) {
-        paramValue = app;
-      } else if (isNull(paramValue)) { // Otherwise, check if the param name matches any dependency
-
-        // Trying to find any dependency with the same name
-        const service = findServiceByName(app, toLower(key));
-
-        // If was found
-        if (service != null) {
-          // And it gots and instance, assign the instance
-          if (service.instance) {
-            paramValue = service.instance as Service;
-          } else { // Otherwise, creates a new instance, and assign
-            paramValue = newInstace(app, service.clazz, service.args);
-            // Setting the instance if it's singleton
-            service.instance = service.isSingleton ? paramValue : null;
-          }
-        }
-      }
 
       // Setting the param name and value
       data[paramName] = paramValue;
@@ -143,33 +88,27 @@ export default (function IoC() {
     }) as Service;
   };
 
-  /**
-   * Destroy an instance registered
-   * @param app the application
-   */
   const clear = (app: Bouer) => {
     return serviceCollection.delete(app);
   };
 
-  class IoC {
+  return {
     /**
      * Defines the bouer app containing all the services that needs to be provided in this app
      * @param app the bouer instance
      * @returns all the available methods to perform
      */
-    static app<Data extends {} = {}, GlobalData extends {} = {}, Dependencies extends {} = {}>(
+    app<Data extends {} = dynamic, GlobalData extends {} = dynamic, Dependencies extends {} = dynamic>(
       app: Bouer<Data, GlobalData, Dependencies>
     ) {
       return {
         /**
          * Adds a service to be provided in whole the app
          * @param clazz the service that should be resolved future on
-         * @param params the static parameter that needs to be resolved every time the service is requested.
-         *  Struture: { parameterExactName: parameterValue }. In case os current bouer instance, it is already
-         *  injected no need to specify
+         * @param params the parameter that needs to be resolved every time the service is requested.
          * @param isSingleton mark the service as singleton to avoid creating an instance whenever it's requested
          */
-        add<Service>(clazz: ServiceClass<Service>, params?: dynamic, isSingleton?: boolean) {
+        add<Service>(clazz: ServiceClass<Service>, params?: unknown[], isSingleton?: boolean): void {
           return add(app as Bouer, clazz, params, isSingleton);
         },
         /**
@@ -177,7 +116,7 @@ export default (function IoC() {
          * @param clazz the class the needs to be resolved
          * @returns the instance of the class resolved
          */
-        resolve<Service>(clazz: (new (...args: any[]) => Service)) {
+        resolve<Service>(clazz: (new (...args: any[]) => Service)): Service | null {
           return resolve(app as Bouer, clazz);
         },
         /**
@@ -187,30 +126,29 @@ export default (function IoC() {
           clear(app as Bouer);
         }
       };
-    }
+    },
 
     /**
      * Creates a new instance of a class provided
      * @param clazz the class that the new instance should be created
+     * @param params the parameter list that will be injected in the constructor
      * @returns new intance of the class provided
      */
-    static new<Service>(clazz: (new (...args: any[]) => Service), params?: dynamic) {
+    new<Service>(clazz: (new (...args: any[]) => Service), params?: unknown[]): Service | null {
 
       if (clazz instanceof Bouer) {
         Logger.error('Cannot create an instance of Bouer using IoC');
-        return;
+        return null;
       }
-      return newInstace(null, clazz, params);
-    }
+      return newInstance(clazz, params);
+    },
 
     /**
      * Generates a unique Id for the application
      * @returns The next integer from the last one generated
      */
-    static newId() {
+    newId(): number {
       return bouerId++;
     }
-  }
-
-  return IoC;
+  };
 })();
