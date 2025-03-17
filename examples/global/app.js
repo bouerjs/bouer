642a1a3ebcabfe699ce59d830b4145f77ac722ca
AfonsoMatElias <afonsomatumona@hotmail.com>
2025-03-17 11:58:36 +0100

    testing changes

--- examples/global/app.js
+++ examples/global/app.js
@@ -2,5 +2,13 @@ import Bouer from "../../dist/bouer.esm.js";
 import AppMain from "./components/main/app-main.js";
 
 new Bouer('#app', {
-  components: [ AppMain ]
+  components: [ AppMain ],
+  pipes: {
+    upper(value) {
+      return value.toUpperCase();
+    },
+    currency(value, p1) {
+      return p1 + ' ' + value;
+    }
+  }
 });
