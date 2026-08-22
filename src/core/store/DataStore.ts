90ed99216d37db40f238e819bcfb8d668f7e2cc0
AfonsoMatElias <afonsomatumona@hotmail.com>
2026-08-22 13:45:36 +0100

    feat: added memory data store for compiled elements, meaning that everytime the element is compiled it shall have the same data

--- src/core/store/DataStore.ts
+++ src/core/store/DataStore.ts
@@ -1,5 +1,7 @@
+import INode from '../../definitions/interfaces/INode';
 import dynamic from '../../definitions/types/Dynamic';
 import RenderContext from '../../definitions/types/RenderContext';
+import { forEach } from '../../shared/helpers/Utils';
 import Logger from '../../shared/logger/Logger';
 
 export default class DataStore {
@@ -16,6 +18,12 @@ export default class DataStore {
   data: dynamic = {};
   req: dynamic = {};
 
+
+  element = {
+    data: new WeakMap<Element, dynamic>(),
+    keys: new Array<Element>()
+  }
+
   set<TKey extends keyof DataStore>(key: TKey, dataKey: string, data: object) {
     if (key === 'wait') return Logger.warn('Only “get” is allowed for type of data');
     (this as { [k: string]: any })[key][dataKey] = data;
@@ -29,5 +37,28 @@ export default class DataStore {
 
   unset<TKey extends keyof DataStore>(key: TKey, dataKey: string) {
     delete (this as { [k: string]: any })[key][dataKey];
+  }
+
+  getNodeData(element: Element | Node, defaultData?: dynamic) {
+    if (!this.element.data.has(element as Element))
+      return defaultData;
+
+    return this.element.data.get(element as Element) ?? defaultData;
+  }
+
+  addNodeData(element: Element, data: dynamic) {
+    this.element.data.set(element, data);
+    this.element.keys.push(element);
+  }
+
+  unlinkNodeData() {
+    forEach(this.element.keys, (el:INode) => {
+      const isUnlinked = !el.isConnected || (el.isActive || (() => { return false; }))();
+
+      if (!isUnlinked)
+        return;
+
+      this.element.data.delete(el as Element);
+    });
   }
 }
