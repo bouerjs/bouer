980a6e156829e11e7734656b0f8e36ee6e1231ac
AfonsoMatElias <afonsomatumona@hotmail.com>
2026-08-22 11:21:15 +0100

    feat: introduction of FormHandling with validation, among with e-form and e-schema directive

--- examples/html-to-obj/app.js
+++ examples/html-to-obj/app.js
@@ -4,13 +4,62 @@ var app = new Bouer('#app', {
     path: '/components/input-field.html'
   }],
   data: {
+    personForm: {
+      schema: {
+        firstName: {
+          required: true,
+          length: { min: 3, max: 10 },
+          // value: 'Afonso'
+        },
+        lastName: {
+          required: true,
+          length: { min: 3, max: 10 },
+          // value: 'Matumona'
+        },
+        birthdate: {
+          required: true,
+          type: 'string',
+          pattern: 'date',
+          // value: '1999-01-01'
+        },
+        email: {
+          required: true,
+          pattern: 'email',
+          // value: 'mail@example.com',
+        },
+        phones: [
+          {
+            code: {
+              required: true,
+              length: 2,
+              // value: 'AO'
+            },
+            phoneNumber: {
+              required: true,
+              length: 9,
+              // value: '123456789'
+            }
+          }
+        ],
+        identification: {
+          type: {
+            required: true,
+            check: ['ID', 'PP', 'DL'],
+            // value: 'DL'
+          },
+          code: {
+            required: true,
+            length: 9,
+            // value: '924456789'
+          }
+        }
+      }
+    },
     submit: function () {
-      for ( item of this.$components.viewByName('input-field'))
-        if (!item.data.value)
-          item.data.isValid = false
+      console.log('Compiled Form', this.data.personForm);
 
-      return;
       var form = document.querySelector('form');
+
       console.log(app.toJsObj(form));
       document.querySelector("#output").textContent = "Check the output in browser console"
     }
